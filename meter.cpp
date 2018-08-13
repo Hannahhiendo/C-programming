60bd2db13cf39d04f7e3b26ef44d68a6d736489f
Hannah Do <marinahiendo@gmail.com>
2018-08-13 14:13:08 -0400

    converting feet into meters

---
+++ meter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    double feet;
+    cout<< "Enter a value for feet: ";
+    cin >> feet;
+
+    double meters = feet * 0.305;
+    cout << feet<< " feet is "<< meters << " meters" << '\n';
+
+    
+}
