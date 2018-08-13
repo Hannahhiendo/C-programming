6d57827b757f302f9348b067cb288539cfbc73f7
Hannah Do <marinahiendo@gmail.com>
2018-08-13 13:37:19 -0400

    converting celsius degrees to farenheit

---
+++ celsiusfar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+double celsius;
+cout << "Enter a Celsius Degree" << '\n';
+cin >> celsius;
+
+
+int farenheit = (9.0/5) * celsius + 32;
+
+
+cout << " The Farenheit Degree is " << farenheit << '\n';
+
+
+
+}
