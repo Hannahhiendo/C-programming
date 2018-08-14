6268aeb67f122a0c849fe755e2945e1217542c29
Hannah Do <marinahiendo@gmail.com>
2018-08-14 15:33:41 -0400

    c2;2.11*

---
+++ accumulated.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "Enter investment amount: "<< '\n';
+     double investmentAmount;
+    cin >> investmentAmount;
+    cout << "Enter anual interest rate: "<< '\n';
+     double anualInterestRate;
+    cin >> anualInterestRate;
+    cout  << "Enter number of years: "<<'\n';
+    double numberOfYears;
+    cin >> numberOfYears;
+    double monthlyInterestRate = anualInterestRate/12;
+    monthlyInterestRate = monthlyInterestRate/100;
+   
+    
+    double accumulatedValue = investmentAmount * pow(1 + monthlyInterestRate, numberOfYears * 12);
+
+    cout << "The Accumulated Value is $"<< accumulatedValue << '\n'; 
+
+    
+
+ 
+    return 0;
+
+
+}
