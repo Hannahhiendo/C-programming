3cf364fcbbd40ff5182622c5dda4641d71b1982a
Hannah Do <marinahiendo@gmail.com>
2018-08-15 13:05:28 -0400

    c2;2.8*

---
+++ charactercode.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int integer;
+    cout << "Enter an ASCII code: ";
+    cin >> integer;
+ 
+    char c= (char)integer;
+    
+
+
+   
+    
+    cout << c << '\n';
+    return 0;
+   
+    
+}
