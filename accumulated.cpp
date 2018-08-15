d149e557bd958a675c9b780d1c8964174c16e55f
Hannah Do <marinahiendo@gmail.com>
2018-08-15 12:56:31 -0400

    anual changed to annual

--- accumulated.cpp
+++ accumulated.cpp
@@ -8,13 +8,15 @@ int main()
      double investmentAmount;
     cin >> investmentAmount;
     cout << "Enter anual interest rate: "<< '\n';
-     double anualInterestRate;
-    cin >> anualInterestRate;
+     double annualInterestRate;
+    cin >> annualInterestRate;
     cout  << "Enter number of years: "<<'\n';
     double numberOfYears;
     cin >> numberOfYears;
-    double monthlyInterestRate = anualInterestRate/12;
+        double monthlyInterestRate = annualInterestRate/12;
     monthlyInterestRate = monthlyInterestRate/100;
+
+    
    
     
     double accumulatedValue = investmentAmount * pow(1 + monthlyInterestRate, numberOfYears * 12);
