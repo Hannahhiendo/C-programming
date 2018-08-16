ffdb07a29f8b13f05c4a5c3c66a16850310d065a
Hannah Do <marinahiendo@gmail.com>
2018-08-16 11:46:12 -0400

    c2;2.12*

---
+++ 2currentamount.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double annualInterestRate = 5;
+    annualInterestRate = annualInterestRate / 100;
+    double monthlyInterestRate = annualInterestRate / 12;
+
+    double currentAmount = 0;
+    int a = 1;
+    for (a =1; a<7; a++) {
+    currentAmount = (100 + currentAmount) * (1 + monthlyInterestRate);
+   
+  }
+
+   cout << currentAmount << '\n';
+
+    
+}
