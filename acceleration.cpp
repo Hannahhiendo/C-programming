9a6e43d3fc37cfa47b72bf06fc6956c05568808e
Hannah Do <marinahiendo@gmail.com>
2018-08-17 12:05:08 -0400

    c2;2.20

---
+++ acceleration.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+cout << "Enter starting velocity:  ";
+double startingVelocity;
+cin >> startingVelocity;
+cout << "Enter ending velocity: ";
+double endingVelocity;
+cin >> endingVelocity;
+cout << "Enter Time Span: ";
+double timeSpan;
+cin >> timeSpan;
+
+
+double averageAcceleration = (endingVelocity - startingVelocity) / timeSpan;
+
+cout << "The Average Acceleration is " << averageAcceleration << '\n';
+
+return 0;
+
+
+
+
+
+
+}
