bffb8262330d5c2d0c29108d405b5842cb749604
Hannah Do <marinahiendo@gmail.com>
2018-08-18 13:52:00 -0400

    c3;3.2

---
+++ booleanvalue.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    int x = 1;
+    x = x != 1;
+
+    cout << x << '\n';
+
+    cout << (4 > 5) << '\n';
+
+
+
+    return 0;
+
+
+
+}
