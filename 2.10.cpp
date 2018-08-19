6375a58b08c556d9f201364228312cfc5181e519
Hannah Do <marinahiendo@gmail.com>
2018-08-18 22:44:53 -0400

    arithmetic expressions

---
+++ 2.10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main ()
+{
+
+int a;
+int b;
+int c;
+int d;
+int r;
+int t;
+
+// (a)
+cout << 4 / 3 * (r + 34) - 9 * (a + b * c) + 3 + d * (2 + a) / a + b * d<< '\n';
+// (b)
+cout << 5.5 * pow(r + 2.5, 2.5 + t)<< '\n';
+
+return 0;
+
+
+}
