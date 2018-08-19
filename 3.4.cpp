c933c39fbbc852e2bafba6b1f25a05e677d8e004
Hannah Do <marinahiendo@gmail.com>
2018-08-19 18:40:41 -0400

    even or odd

---
+++ 3.4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int number = 6;
+    if (number % 2 == 0)
+   { 
+       cout << number << " is even";
+    }
+    
+    else
+    { 
+        cout << number <<" is odd";
+    }
+   
+   
+
+
+
+    return 0;
+
+}
