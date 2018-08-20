a9e00d29b544b725128b59e2bb2ca3241d931421
Hannah Do <marinahiendo@gmail.com>
2018-08-20 17:48:04 -0400

    nesting

---
+++ 3.6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int x = 3;
+    int y = 2;
+    int z = x + y;
+
+    if ( x > 2)
+    {
+         if( y > 2)
+         cout << "z is "<< z << '\n';
+
+
+          else{
+        cout << "x is " << x << '\n';
+    }
+    }
+
+   
+    
+
+
+    return 0;
+
+}
