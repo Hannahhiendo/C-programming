d0947e9cbf48cf746821fb3941548e22162b70ba
Hannah Do <marinahiendo@gmail.com>
2018-08-20 20:15:54 -0400

    what's your grade

---
+++ 3.10.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+
+int main ()
+{
+char grade;
+double score;
+cout << "enter test score: ";
+cin >> score;
+    
+    if (score >= 90)
+        grade = 'A';
+    else if (score >= 80)
+       grade = 'B';
+    else if (score >= 70)
+        grade = 'C';
+    else if (score >= 60)
+        grade = 'D';
+    else
+        grade = 'F';
+        
+        
+    cout << grade << '\n';
+
+    
+
+
+       
+    
+    
+return 0;
+
+}
