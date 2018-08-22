52e5699c24e07d978246560c6a14b506c780d782
Hannah Do <marinahiendo@gmail.com>
2018-08-22 16:45:13 -0400

    chapter 3

---
+++ chapter_3/lottery.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main (){
+    srand(time (0));
+    int lottery = rand() % 100;
+    
+
+    cout << "Enter your lottery pick (two digits)" << '\n';
+    int guess;
+    cin >> guess;
+
+    int lotteryDigit1 = lottery / 10;
+    int lotteryDigit2 = lottery % 10;
+
+    int guessDigit1 = guess / 10;
+    int guessDigit2 = guess % 10;
+
+    if(guess == lottery){
+        cout << "Exact match : you win $10,000" << '\n';
+    }
+
+    else if (guessDigit1 == lotteryDigit2 && guessDigit2 == lotteryDigit1){
+        cout << "All digits matched : you win $3,000" << '\n';
+    }
+
+    else if (guessDigit1 == lotteryDigit1 || guessDigit1 == lotteryDigit2 || guessDigit2 == lotteryDigit1 || guessDigit2 == lotteryDigit2){
+        cout << "One digit matched : you win $1000" << '\n';
+    }
+    
+    else {
+        cout << "no match, thanks for playing" << '\n';
+    }
+
+    cout << lottery<< '\n';
+
+    return 0;
+}
