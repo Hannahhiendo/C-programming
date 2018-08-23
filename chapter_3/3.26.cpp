1ef99b429c14b5850bbbfde233994fc4fcd0000e
Hannah Do <marinahiendo@gmail.com>
2018-08-23 18:12:57 -0400

    chapter 3

---
+++ chapter_3/3.26.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+    cout << setw(10) << "C++" << setw(6) << 101 << '\n';
+    cout << setw(8) << "Java" << setw(5) << 101 << '\n';
+    cout << setw(6) << "HTML" << setw(4) << 101 << '\n';
+
+return 0;
+}
