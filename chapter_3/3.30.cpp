0951fced9650bfdf382a5c13dbb52e0a12bf3309
Hannah Do <marinahiendo@gmail.com>
2018-08-24 18:09:56 -0400

    chapter 3

---
+++ chapter_3/3.30.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main (){
+    cout << left;
+    cout << setw(6) << 21.23 << '\n';
+    cout << setw(6) << 51.34 << '\n';
+
+    return 0;
+}
