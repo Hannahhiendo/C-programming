16f72b40c9aef6b4e1c61383686b9b4573f1bed8
Hannah Do <marinahiendo@gmail.com>
2018-08-24 18:11:20 -0400

    chapter 3

--- chapter_3/3.30.cpp
+++ chapter_3/3.30.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int main (){
-    cout << left;
-    cout << setw(6) << 21.23 << '\n';
-    cout << setw(6) << 51.34 << '\n';
+cout << left;
+cout << setw(6) << 21.23 << '\n';
+cout << setw(6) << 51.34 << '\n';
 
     return 0;
 }
