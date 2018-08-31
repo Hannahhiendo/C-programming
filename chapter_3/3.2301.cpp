75ea527b1b7f4eea39c8780fe39bdf79e0f387b1
Hannah Do <marinahiendo@gmail.com>
2018-08-31 15:36:22 -0400

    chapter 3

---
+++ chapter_3/3.2301.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ cout << "Enter threee edges: ";
+ double edge1, edge2, edge3;
+ cin >> edge1;
+ cin >> edge2;
+ cin >> edge3;
+cout << "Can edges " << edge1 << ", " << edge2 << ", " << edge3 << "form a triangle: ";
+
+if(edge1 + edge2 > edge3 || edge1 + edge3 > edge2 || edge2 + edge1 > edge3 || edge2 + edge3 > edge1 || edge3 + edge1 > edge2 || edge3 + edge2 > edge1){
+    cout << "true" << '\n';
+}
+else {
+    cout << "false" << '\n';
+}
+}
