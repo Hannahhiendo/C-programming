337dd406d8b3f9b811ce4a943b1250c68b2ffebf
Hannah Do <marinahiendo@gmail.com>
2018-09-08 20:29:31 -0400

    chapter 4

--- chapter_4/subtractionquizloop.cpp
+++ chapter_4/subtractionquizloop.cpp
@@ -5,40 +5,41 @@ using namespace std;
 
 int main ()
 {
-int correctCount = 0;
-int count = 0;
-long startTime = time(0);
-int numberOfQuestions = 5;
 char continueLoop = 'Y';
 while (continueLoop == 'Y'){
-while (count < numberOfQuestions){
-srand(time(0));
-int number1 = rand() % 10;
-int number2 = rand() % 10;
+    int correctCount = 0;
+    int count = 0;
+    long startTime = time(0);
+    int numberOfQuestions = 5;
 
-if (number1 < number2) {
-    int peanut = number1;
-    number1 = number2;
-    number2 = peanut;
-}
+    while (count < numberOfQuestions){
+        srand(time(0));
+        int number1 = rand() % 10;
+        int number2 = rand() % 10;
 
-cout << "what is " << number1 << " - " << number2 << "? ";
-int answer;
-cin >> answer;
+        if (number1 < number2) {
+            int peanut = number1;
+            number1 = number2;
+            number2 = peanut;
+        }
 
-if (number1 - number2 == answer){
-    cout << "You are correct"<< '\n';
-    correctCount++;
-}
-else {
-    cout << "Your answer is wrong\n" << number1 <<" - "<< number2 << " should be " << (number1 - number2 ) << '\n';
-}
+        cout << "what is " << number1 << " - " << number2 << "? ";
+        int answer;
+        cin >> answer;
 
-count++;
+        if (number1 - number2 == answer){
+            cout << "You are correct"<< '\n';
+            correctCount++;
+        }
+        else {
+            cout << "Your answer is wrong\n" << number1 <<" - "<< number2 << " should be " << (number1 - number2 ) << '\n';
+        }   
+
+        count++;
 }
 long endTime = time(0);
 long testTime = endTime - startTime;
-cout << "Correct count is " << correctCount << "\nTest time is " << testTime << " sesonds\n";
+cout << "Correct count is " << correctCount << " out of 5" << "\nTest time is " << testTime << " sesonds\n";
 
 cout << "Enter Y to continue and N to quit" << '\n';
 cin >> continueLoop;
