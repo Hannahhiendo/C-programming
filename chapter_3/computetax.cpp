0b25b68f625dec807251e54b8a358980a010650d
Hannah Do <marinahiendo@gmail.com>
2018-09-14 17:39:07 -0400

    chapter_3

--- chapter_3/computetax.cpp
+++ chapter_3/computetax.cpp
@@ -43,6 +43,48 @@ int main()
             tax = income * 0.10;
         }
         else if (income <= 46700){
+            tax = 12000 * 0.10 + (income - 12000) * 0.15;
+        }
+        else if (income <= 112850){
+            tax = 12000 * 0.10 + (income - 12000) * 0.15 + (income - 46700) * 0.27;
+        }
+        else if (income <= 171950){
+            tax = 12000 * 0.10 + (income - 12000) * 0.15 + (income - 46700) * 0.27 + (income - 46700) * 0.30;
+        }
+        else if (income <= 307050){
+            tax = 12000 * 0.10 + (income - 12000) * 0.15 + (income - 46700) * 0.27 + (income - 112850) * 0.30 + (income - 171950) * 0.35;
+        }
+        else {
+            tax = 12000 * 0.10 + (income - 12000) * 0.15 + (income - 46700) * 0.27 + (income - 112850) * 0.30 +(income - 171950) * 0.35 + (income - 307050) * 0.386;
+        }
+    }
+
+        if (status == 1){
+        if (income <= 6000){
+            tax = income * 0.10;
+        }
+        else if (income <= 23350){
+            tax = 6000 * 0.10 + (income - 6000) * 0.15;
+        }
+        else if (income <= 112850){
+            tax = 6000 * 0.10 + (income - 6000) * 0.15 + (income - 23350) * 0.27;
+        }
+        else if (income <= 171950){
+            tax = 6000 * 0.10 + (income - 6000) * 0.15 + (income - 23350) * 0.27 + (income - 67700) * 0.30;
+        }
+        else if (income <= 307050){
+            tax = 6000 * 0.10 + (income - 6000) * 0.15 + (income - 23350) + (income - 67700) * 0.30 + (income - 141250) * 0.35;
+        }
+        else {
+            tax = 6000 * 0.10 + (income - 6000) * 0.15 + (income - 23350) + (income - 67700) * 0.30 +(income - 141250) * 0.35 + (income - 307050) * 0.386;
+        }
+    }
+
+        if (status == 1){
+        if (income <= 12000){
+            tax = income * 0.10;
+        }
+        else if (income <= 46700){
             tax = 12000 * 0.10 + (income - 6000) * 0.15;
         }
         else if (income <= 112850){
