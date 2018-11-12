5263889d549544c6115a9bd017a0c692bfd76d1b
Hannah Do <marinahiendo@gmail.com>
2018-11-11 22:05:51 -0500

    testcode

---
+++ testcode/test7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream out("table.txt");
+    for(int a=1;a<11;a++)
+    {
+        for(int k=1;k<11;k++)
+        {
+            out<<a<<'x'<<k<<'='<<a*k<<endl;
+        }
+    }
+    ifstream in("table.txt");
+    out.close();
+    return 0;
+}
