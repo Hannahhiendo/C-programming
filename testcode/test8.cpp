8a6585aabc815894e4009f7ae90213018b006eae
Hannah Do <marinahiendo@gmail.com>
2018-11-20 22:43:00 -0500

    testcode

---
+++ testcode/test8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter a fahrenheit"<< '\n';
+    int fahrenheit;
+    cin >> fahrenheit;
+    int celsius;
+    celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
+    cout << celsius;
+}
