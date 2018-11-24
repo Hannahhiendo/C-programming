dfbded312d2a28d5f6541af5f48c087aabc035e9
Hannah Do <marinahiendo@gmail.com>
2018-11-23 22:42:48 -0500

    chapter_5

---
+++ chapter_5/5.10.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+//double computerComission(double salesAmount){
+    
+
+//int subtractionVariable = 9100;
+//double comission = salesAmount - subtractionVariable;
+///return comission;
+
+
+    
+
+
+
+  
+//}
+
+int main(){
+double salesAmount = 10000;
+double comission = 900;
+
+cout << "Sales Amount" << '\t' << "Comission" << '\n'; 
+for(int n = 1; n <= 20; n++){
+
+cout << salesAmount << '\t' << comission << '\n';
+
+salesAmount = salesAmount + 5000;
+comission = comission + 600;
+//int subtractionVariable = subtractionVariable + 4400;
+}
+
+}
