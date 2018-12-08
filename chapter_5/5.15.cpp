93fb1886afe85835e78cf9f217d3873ed5d819e4
Hannah Do <marinahiendo@gmail.com>
2018-12-07 22:23:39 -0500

    chapter_5

--- chapter_5/5.15.cpp
+++ chapter_5/5.15.cpp
@@ -3,20 +3,27 @@ using namespace std;
 
 
 int numberOfDaysInAYear(int year);
+int numberOfDaysInLeapYear(int leap);
 
 int main(){
 
     for(int year = 2000; year <= 2010; year++){
-        cout << year << '\t' << numberOfDaysInAYear(year) << '\n';
+      
+        if(year/4 != 0){
+            int leap;
+            cout << year << '\t' << numberOfDaysInLeapYear(leap) << '\n';
+        }
+        
+        else{
+            cout << year << '\t' << numberOfDaysInAYear(year) << '\n';
+        }
     }
 }
 
 int numberOfDaysInAYear(int year){
-    
-    if(year/4 != 0){
-        return 356;
-    }
-    if(year/4 == 0){
-        return 366;
-    }
+     return 356;
+ 
+}
+int numberOfDaysInLeapYear(int leap){
+    return 366;
 }
