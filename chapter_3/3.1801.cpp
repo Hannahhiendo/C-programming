05ee6b18f500863e1251be17590d3cf470c05d97
Hannah Do <marinahiendo@gmail.com>
2018-12-13 22:58:56 -0500

    chapter 3

--- chapter_3/3.1801.cpp
+++ chapter_3/3.1801.cpp
@@ -18,6 +18,8 @@ TEST(DaysTest, PositiveNos) {
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+
+  return 0;
 }
 
 int getDays(int year, int month){
